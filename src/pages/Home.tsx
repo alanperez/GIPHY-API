67771abfcb64a9f34534059a679fe949e3b9a0b4
perezxalan <perezxalan@gmail.com>
2022-07-08 15:39:13 -0500

    setting up Homepage that will hold our comp

---
+++ src/pages/Home.tsx
@@ -0,0 +1,21 @@
+
+
+
+
+export default function Home() {
+
+
+    return (
+        <div className="home-container">
+
+            {/* featured / trending cards */}
+            <div className="card-container__title">
+                <h2 className="home-text">🔥Trending Giphys🔥</h2>
+            </div>
+            <div className="home-trending-card">
+                {/* Cards */}
+            </div>
+        </div>
+    )
+
+}
